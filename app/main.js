82fe5d9de0447e48d441460e32738523ec8c37be
rheamendis <mendisrhea@gmail.com>
2016-06-26 20:35:37 +0530

    Alert on invalid name

--- app/main.js
+++ app/main.js
@@ -1,10 +1,28 @@
+//pop up the form on button click
 $(document).ready(function(){
    $(".container-s").hide();
 
     $("#btn-s").click(function () {
         $(".container-s").show();
     });
+
+
 });
 
-  
+
+
+
+function validate (f)
+{
+    var patt_name = /^[A-Za-z0-9]{3,15}$/;
+    //^[A-Za-z0-9]{3,15}$/;
+    var name =f.fname.value;
+
+    if (!patt_name.test(name)) {
+    alert("Invalid Name");
+        Document.getElementById('ename').style.visibility='hidden';
+    }
+
+}
+
 
